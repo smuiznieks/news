815f1bacf46862843d90eb4c59be19a6f7f3e382
Selga Muiznieks <selga.muiznieks@gmail.com>
2018-03-10 10:24:12 -0500

    edit server

--- server.js
+++ server.js
@@ -21,11 +21,13 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static('public'));
 
 // If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
-var MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/mongoHeadlines';
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 // Set mongoose to leverage built in JavaScript ES6 Promises
 // Connect to the Mongo DB
 mongoose.Promise = Promise;
-mongoose.connect(MONGODB_URI);
+mongoose.connect(MONGODB_URI, {
+  useMongoClient: true
+});
 
 // Routes
 app.get('/', function (req, res) {
